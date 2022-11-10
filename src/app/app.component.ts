c6cfc513e33d5812af1090641fd498509059304c
riverdev <yariv.rivercybe@gmail.com>
2022-11-10 15:02:13 +0200

    Added same wireup for the even numbers as i did for the odd numbers, added style color to fonts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   oddNumberList: number[] = [];
+  evenNumberList: number[] = [];
 
-  onIntervalFired(counter: number){
+  onIntervalFired(counter: number) {
     console.log(`counter=${counter}`);
-    if (counter % 2 !== 0 /*Case of odd number */) {
+    if (counter % 2 === 0 /*Case of even number */) {
+      this.evenNumberList.push(counter);
+    } else {
       this.oddNumberList.push(counter);
     }
   }
